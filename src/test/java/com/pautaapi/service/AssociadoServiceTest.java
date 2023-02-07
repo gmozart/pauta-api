11f9a021ea2d607ad19f8125c66a5fd963389d84
gmozart <mozart.gleison@gmail.com>
2023-02-07 19:42:15 -0300

    Criada a classe AssociadoServiceTest para testes unitários da classe AssociadoService.

--- src/test/java/com/pautaapi/service/AssociadoServiceTest.java
+++ src/test/java/com/pautaapi/service/AssociadoServiceTest.java
@@ -3,11 +3,13 @@ package com.pautaapi.service;
 import com.pautaapi.dto.AssociadoDTO;
 import com.pautaapi.entity.Associado;
 import com.pautaapi.repository.AssociadoRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collections;
@@ -15,17 +17,33 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
 class AssociadoServiceTest {
 
+    private static final Long ID = 1l;
+    private  static final String CPF = "14523548485";
+
     @InjectMocks
     private AssociadoService associadoService;
 
     @Mock
     private AssociadoRepository associadoRepository;
+
+    private Associado associado;
+    private  AssociadoDTO associadoDTO;
+    private Optional<AssociadoDTO> optionalAssociado;
+
+    @BeforeEach
+    void setUp(){
+
+        starterAssociado();
+
+    }
+
 
     @Test
     void whenSaveAssociadoTest() {
@@ -37,8 +55,11 @@ class AssociadoServiceTest {
 
     @Test
     void whenFindByIdTest() {
-        when(associadoRepository.findById(any())).thenReturn(Optional.of(Associado.builder().build()));
-        assertNotNull(associadoService.findById(1l));
+        when(associadoRepository.findById(anyLong())).thenReturn(Optional.of(AssociadoDTO.of(associadoDTO)));
+        Optional<AssociadoDTO> response = associadoService.findById(ID);
+        assertNotNull(response);
+        assertEquals(Optional.class, response.getClass());
+        assertEquals(ID, response.get().getId());
     }
 
     @Test
@@ -50,13 +71,17 @@ class AssociadoServiceTest {
     @Test
     void whenUpdateTest() {
 
-
-
-
-
     }
 
     @Test
     void delete() {
     }
+
+    private void starterAssociado(){
+        associado = new Associado(ID, CPF);
+        associadoDTO = AssociadoDTO.of(new Associado(ID,CPF));
+        optionalAssociado = Optional.of(AssociadoDTO.of(new Associado(ID,CPF)));
+    }
+
+
 }
