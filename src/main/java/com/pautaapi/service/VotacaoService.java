7858bc242aecd7448d5afcb8507126f2d2204eee
gmozart <mozart.gleison@gmail.com>
2023-02-10 18:04:15 -0300

    Log da classe votacaoService finalziado

--- src/main/java/com/pautaapi/service/VotacaoService.java
+++ src/main/java/com/pautaapi/service/VotacaoService.java
@@ -9,6 +9,7 @@ import com.pautaapi.repository.PautaRepository;
 import com.pautaapi.repository.VotacaoRepository;
 import com.pautaapi.util.ResponseApi;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -17,6 +18,7 @@ import java.util.Optional;
 
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class VotacaoService {
 
@@ -30,26 +32,33 @@ public class VotacaoService {
         Pauta pauta = pautaRepository.findById(votacaoDTO.getPautaId()).get();
         Associado associado = associadoRepository.findById(votacaoDTO.getAssociadoId()).get();
         if(LocalDateTime.now().isAfter(pauta.getFechamento())){
+            log.info("Voto não salvo a pauta está expirada.");
             return ResponseApi.builder().body("A pauta esta expirada!").build();
         }
         Votacao votacao = Votacao.builder().id(votacaoDTO.getId()).pauta(pauta).associado(associado).snVoto(votacaoDTO.getSnVoto()).build();
         votacaoRepository.save(votacao);
+        log.info("Voto salvo e computado");
         return ResponseApi.builder().body("Voto computado com sucesso!").build();
     }
     public Optional<VotacaoDTO> findById(Long id) {
+        log.info("Busca dos votos pelo id");
         return VotacaoDTO.of(votacaoRepository.findById(id));
     }
     public Optional<List<VotacaoDTO>> findAll() {
+        log.info("Retorna uma lista de votos.");
         return Optional.of(VotacaoDTO.of(votacaoRepository.findAll()));
     }
 
     public Integer numberVotesInFavor(Long id) {
+        log.info("Retorna quantidade de votos afavor da pauta");
         return votacaoRepository.numberVotesInFavor(id);
     }
     public Integer numberVotesAgainst(Long id) {
+        log.info("Retorna quantidade de votos contra da pauta");
         return votacaoRepository.numberVotesAgainst(id);
     }
     public Integer numberofVotes(Long id) {
+        log.info("Retorna quantidade de votos de uma pauta.");
         return votacaoRepository.numberOfVotes(id);
     }
 }
